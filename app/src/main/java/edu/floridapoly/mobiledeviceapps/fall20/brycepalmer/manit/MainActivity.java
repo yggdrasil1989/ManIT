a827c402c451ff5886b27752f82b116007f73e50
Bryce Palmer <everettraven@gmail.com>
2020-10-06 14:07:39 -0400

    Initial Commit

---
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall20/brycepalmer/manit/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.floridapoly.mobiledeviceapps.fall20.brycepalmer.manit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
