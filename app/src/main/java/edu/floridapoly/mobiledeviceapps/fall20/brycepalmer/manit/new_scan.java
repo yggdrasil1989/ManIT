1ca16da73c24cd5871b6d7f9e684933013db0945
liefw <liefwatts@gmail.com>
2020-10-25 18:12:16 -0400

    modified the wifi scan app

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall20/brycepalmer/manit/new_scan.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall20/brycepalmer/manit/new_scan.java
@@ -1,21 +1,26 @@
 package edu.floridapoly.mobiledeviceapps.fall20.brycepalmer.manit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.net.wifi.WifiInfo;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class new_scan extends AppCompatActivity {
 
+    String networkname;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_scan);
     }
 
-    public void clicked_add_WAP(View view) {
+    public void clicked_scan_WAP(View view) {
         Toast.makeText(getApplicationContext(),"Adds WAP data to database", Toast.LENGTH_LONG).show();
+
     }
     public void clicked_validate(View view) {
         Toast.makeText(getApplicationContext(),"Option to validate later", Toast.LENGTH_LONG).show();
